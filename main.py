6aa7dff4b902b41a99aeebb8a7d36da17ef74d20
Ariana Ahmadi <83236136+ArianaAhmadi@users.noreply.github.com>
2021-06-11 17:49:33 +0000

    Lab#3

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
 '''
 #Function 1
-def two_lists(n):
-  list_1 = []
-  list_2 = []
-  for i in n:
-    if i.isdigit() == True:
-      list_1.append(i)
-    else:
-      list_2.append(i)
-  print("Numbers List:")
-  print(list_1)
-  print("Words List:")
-  return list_2
+def two_lists(n): #Define the function two_list to n
+  list_1 = [] # Create first list for the numbers
+  list_2 = [] #Create a second list for the word
+  for i in n: #Create a for loop for i in n
+    if i.isdigit() == True: #Create an if statement that's true
+      list_1.append(i) #append() list_1 which adds a single item to the existing list
+    else: #Create you else in the if statement
+      list_2.append(i) #append() list_2 which adds a single item to the existing list
+  print("Numbers List:") #Print a prompt that shows the number list output
+  print(list_1) #Print list 1 which is the list with numbers
+  print("Words List:") #Print a prompt that shows the word list output
+  return list_2 #end the execution(returns) of list_2
 
-print(two_lists([(s) for s in input("Type in any letter and/or number sepereated by spaces:\n").split()]))
+print(two_lists([(s) for s in input("Type in any letter and/or number separated by spaces:\n").split()])) #Prompt the user to input letters or numbers separated by spaces and all placed in one line
 '''
 
 '''
 #Function 2
-def numbers_list(n):
-  for i in range(2, len(n), 2):
-      list_1 = n.sort()
-  return list_1
+def descend(n): # defining the function descend to n
+  even_list = [] #Create a list called even_list
+  length = len(n) #Find the length of n
+  for i in range(length): #create a for loop with a range for length
+    if i * 2 // 2 % 2 == 1: #create an if statement of the remainder of 2 being able to equal 
+      even_list.append(int(n[i])) #append() even_list which adds a single item to the existing list
+  for i in range(length): #create a for loop with a range for length
+    if i * 2 // 2 % 2 == 1: #create an if statement 
+      n[i] = (str(max(even_list))) #this finds the max numbers
+      place = even_list.index(max(even_list)) #search for a given element from the start of the list and returns the lowest index 
+      even_list[place] = 0 #Deletes the max number
+  print("Every second number is now in order:") #
+  return (' '.join(n)) #end the execution(return) and joins n
 
-print(numbers_list([int(s) for s in input("Give list of numbers: ").split()]))
+a = input("Input any number: \n") #Prompt the user to input any number
+s= a.split() #split returns the list of the words in the string/line
+print(descend(s)) # prints the function descen and (s)
 '''
-'''
-going down by one 
-remeber that 1 ex2 a function where one of the return state wsas n*a()-1 something like that do tat for index but
-
-#every second letter replsce with letter then arrange lowest to highest then once done then replace the letter b with th list we rearranged from lowest to highest and boom done
-'''
-
-############################
-#This one is my fave because you can input six OR MORE and it will make it into decending order and we can just make a way where it effects every two, so [::2] or something like that\
-
-def descend(n):
-  even_list = []
-  for i in n:
-    if i % 2 == 0:
-      even_list.append(i)
-  for i in even_list:
-    if i % 2 == 0:
-      new_even_list = 
-      even_list.replace(str(i), max(even_list))
-  return new_even_list
-
-print(descend([int(s) for s in input("Input any number: \n").split()]))
